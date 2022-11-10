ccb80385b9c9c44a45ab0e849ba0ccaf5a3fd202
Evgeny Nagaev <evn1054@gmail.com>
2022-11-10 23:57:38 +0300

    feat: start structure of App, Header, MenuAdmin, Order components

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import MenuAdmin from './MenuAdmin';
+
+class App extends React.Component {
+  render() {
+    return(
+      <>
+        <div className="wine-paradise">
+          <div className="menu">
+            <Header/>
+          </div>
+
+          <Order/>
+          <MenuAdmin/>
+
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return(
+      <header className="top">
+        <div className="wrap">
+          <div className="header-content">
+            <h1>Wine</h1>
+            <p className="header-tagline">Tasting & Pairing</p>
+            
+            <div className="header-divider"></div>
+
+            <div className="header-rating">
+              <div className="header-rating_tag">Рейтинг:</div>
+              <div className="header-rating_icon">+++</div>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
