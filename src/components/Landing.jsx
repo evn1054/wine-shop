9b825a243832921256fa5c5457b7b495eb225ab5
Evgeny Nagaev <evn1054@gmail.com>
2022-11-11 22:21:31 +0300

    feat: add goToShop method in Landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,34 +1,36 @@
 import React from "react";
-import shops from '../sample-shops';
+import shops from "../sample-shops";
 
 class Landing extends React.Component {
-
   state = {
     display: false,
-    title: '',
-    url: '',
-  }
+    title: "",
+    url: "",
+  };
 
   displayList = () => {
     const { display } = this.state;
     this.setState({ display: !display });
-  }
+  };
 
   getTitle = (shop) => {
     const { title, url } = shop;
-    this.setState({title, url, display: false})
-  }
+    this.setState({ title, url, display: false });
+  };
 
   goToShop = () => {
-    console.log('go to shop');
-  }
+    const { url } = this.state;
+    this.props.history.push(`/shop/${url}`);
+  };
 
-  render(){
+  render() {
     return (
       <>
         <div className="shop_select">
           <div onClick={this.displayList} className="shop_select_top">
-            <div className="shop_select_top-header">{this.state.title ? this.state.title : 'Выберите магазин'}</div>
+            <div className="shop_select_top-header">
+              {this.state.title ? this.state.title : "Выберите магазин"}
+            </div>
 
             <div className="arrow_picker">
               <div className="arrow_picker-up"></div>
@@ -36,13 +38,23 @@ class Landing extends React.Component {
             </div>
           </div>
 
-          {this.state.display ? <div className="shop_select_bottom">
-            <ul>
-              {shops.map(shop => <li onClick={() => this.getTitle(shop)} key={shop.id}>{shop.title}</li>)}
-            </ul>
-          </div> : null}
+          {this.state.display ? (
+            <div className="shop_select_bottom">
+              <ul>
+                {shops.map((shop) => (
+                  <li onClick={() => this.getTitle(shop)} key={shop.id}>
+                    {shop.title}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : null}
 
-          {this.state.title && !this.state.display ? <button onClick={this.goToShop} className="shop_select-button">Перейти в магазин</button> : null}
+          {this.state.title && !this.state.display ? (
+            <button onClick={this.goToShop} className="shop_select-button">
+              Перейти в магазин
+            </button>
+          ) : null}
         </div>
       </>
     );
